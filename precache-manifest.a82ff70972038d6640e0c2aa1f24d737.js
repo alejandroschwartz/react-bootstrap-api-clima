119b8f5adb9191d49432bd7ffec29eef10ca0681
Schwartz, Alejandro <alejandromatiassch@gmail.com>
2020-10-03 22:00:34 -0300

    Updates

---
+++ precache-manifest.a82ff70972038d6640e0c2aa1f24d737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1516021a025c3757468b0a0966845bf",
+    "url": "/react-bootstrap-api-clima/index.html"
+  },
+  {
+    "revision": "91c8e5261ee9daf2cebb",
+    "url": "/react-bootstrap-api-clima/static/css/main.82dc0908.chunk.css"
+  },
+  {
+    "revision": "ce931a29dfa49fde8e12",
+    "url": "/react-bootstrap-api-clima/static/js/2.83000fb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bootstrap-api-clima/static/js/2.83000fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c8e5261ee9daf2cebb",
+    "url": "/react-bootstrap-api-clima/static/js/main.41c4c54d.chunk.js"
+  },
+  {
+    "revision": "85a8b6eecf51edd7b3fb",
+    "url": "/react-bootstrap-api-clima/static/js/runtime-main.34299e87.js"
+  }
+]);
